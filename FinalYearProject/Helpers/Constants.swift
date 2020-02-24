4badff1d81f3d3a31a75a9e0900555bd07caf0b2
Esteban Giacobbe <estegiacobbe95@libero.it>
2020-02-24 23:52:02 +0000

    Added a navigation controller to the project

--- FinalYearProject/Helpers/Constants.swift
+++ FinalYearProject/Helpers/Constants.swift
@@ -12,5 +12,6 @@ import Foundation
 struct  Constants {
     struct Storyboard {
         static let homeViewController = "HomeVC"
+        static let tabViewController = "TabViewController"
     }
 }
---
+++ FinalYearProject/View Controllers/TabViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TabViewController.swift
+//  FinalYearProject
+//
+//  Created by Esteban Giacobbe on 24/02/2020.
+//  Copyright © 2020 Esteban Giacobbe. All rights reserved.
+//
+
+import Foundation
